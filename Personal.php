7723ba725b2c099f55891299cd168db5d764f3ca
Gian003 <dev.Rodriguez2111@gmail.com>
2025-05-19 04:01:33 +0800

    update

--- Personal.php
+++ Personal.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>My Portfolio</title>
     <link rel="stylesheet" href="Animation.css">
-    <link rel="stylesheet" type="text/css" href="global.css">
+    <link rel="stylesheet" type="text/css" href="Global.css">
     <link rel="stylesheet" href="Personal_style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
@@ -34,25 +34,72 @@
         <h1>Personal Information</h1>
         <div class="form-wrapper">
              <form action="Personal.php" method="post">
-                <div class="name-section">
-                    <div class="name-container">
-                        <label for="lname">Last Name:</label><br>
+                <div class="input-name-section">
+                    <div class="input-container">
+                        <label>Last Name</label><br>
                         <input type="text" id="lname" name="lname" placeholder="Last Name" required>
                     </div>
 
-                    <div class="name-container">
-                        <label for="fname">First Name:</label><br>
+                    <div class="input-container">
+                        <label>First Name</label><br>
                         <input type="text" id="fname" name="fname" placeholder="First Name" required>
                     </div>
 
-                    <div class="name-container">
-                        <label for="mname">Middle Name:</label><br>
+                    <div class="input-container">
+                        <label>Middle Name</label><br>
                         <input type="text" id="mname" name="mname" placeholder="Middle Name" required>
                     </div>
 
-                    <div class="name-container">
-                        <label for="sname">Suffix:</label><br>
+                    <div class="input-container">
+                        <label>Suffix</label><br>
                         <input type="text" id="sname" name="sname" placeholder="Suffix">
+                    </div>
+                </div>
+
+                <div class="input-address-section">
+                    <h4>Residential Address</h4>
+                    <div class="input-address-container">
+                        <div class="input-container">
+                            <input type="text" id="houseno" name="houseno" placeholder="House No." required>
+                            <label>House No.</label>
+                        </div>
+
+                        <div class="input-container">
+                            <input type="text" id="street" name="street" placeholder="Street" required>
+                            <label>Street</label>
+                        </div>
+
+                        <div class="input-container">
+                            <input type="text" id="barangay" name="barangay" placeholder="Barangay" required>
+                            <label>Barangay</label>
+                        </div>
+
+                        <div class="input-container">
+                            <input type="text" id="city_town" name="city_town" placeholder="City/Town" required>
+                            <label>City/Town</label>
+                        </div>
+
+                        <div class="input-container">
+                            <input type="text" id="province" name="province" placeholder="Province" required>
+                            <label>Province</label>
+                        </div>
+
+                        <div class="input-container">
+                            <input type="text" id="zip_code" name="zip_code" placeholder="Zip Code" required>
+                            <label>Zip Code</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="input-identity-section">
+                    <div class="input-container">
+                        <label>Gender</label>
+                        <input type="radio" id="male" name="gender" value="male" required>Male
+                        <input type="radio" id="female" name="gender" value="female">Female
+                    </div>
+
+                    <div class="input-birth-container">
+
                     </div>
                 </div>
             </form>
